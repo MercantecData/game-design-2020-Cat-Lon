69a33f3d4157ee1fdf766e934a987a9a50712a5b
william <william_kammer@live.dk>
2020-06-15 14:05:13 +0200

    PlayerController + Tiles
    playercontroller med simpel bevægelse og collisions ved ikke helt hvorfor at den ikke bevæger sig uden at hakke. har også tilføjet et tileset med collisions

---
+++ TopDownShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x += speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        position.y += speed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        rigidbody.MovePosition(position);
+    }
+}
