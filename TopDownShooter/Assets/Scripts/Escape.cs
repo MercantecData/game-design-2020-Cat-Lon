a34cbf7c961a9868edca417da1f0868dba49128c
will <William_kammer@live.dk>
2020-06-19 09:45:15 +0200

    Win Condition
    added a boxcollider at the exit of the labyrinth that makes the win text appear

---
+++ TopDownShooter/Assets/Scripts/Escape.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    public GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            winText.SetActive(true);
+        }
+    }
+}
